72cd87863a1262d7ed0a13597e2e21bba0bfc316
Pukuta Mwanza <eliot.alderson20@gmail.com>
2023-07-04 16:57:04 +0200

    first commit

---
+++ src/components/map.js
@@ -0,0 +1,107 @@
+import { React, useState, useEffect } from "react";
+import Head from "./template/head";
+import { Container } from "react-bootstrap";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+import useGetPosts from "./hooks/useGetPosts";
+
+function Explore(props) {
+  const Posts = useGetPosts().docs;
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    // Fetch points from Firestore
+    const fetchPoints = () => {
+      const newPoints = Posts.map((post) => ({
+        latitude: post.latitude || 0,
+        longitude: post.longitude || 0,
+        imageUrl: post.imageUrl || "",
+      }));
+      setPoints(newPoints);
+      console.log("Fetching points : " + JSON.stringify(newPoints));
+    };
+
+    fetchPoints();
+  }, [Posts]);
+
+  // Create an object to track marker positions
+  const markerPositions = {};
+
+  // Function to generate a unique key for a marker position
+  const generateMarkerKey = (lat, lng) => `${lat}-${lng}`;
+
+  // Function to check if a marker position is already occupied
+  const isMarkerOccupied = (lat, lng) => {
+    const key = generateMarkerKey(lat, lng);
+    return markerPositions.hasOwnProperty(key);
+  };
+
+  // Generate a random offset for markers in the same location
+  const generateOffset = () => Math.random() * 0.0023 - 0.001; // Adjust the offset range as needed
+
+  console.log('Generating marker positions' + generateOffset);
+
+  return (
+    <Container fluid>
+      <Head />
+
+      <Container className="d-flex justify-content-center my-1">
+        <h2 className="display-2">Explore</h2>
+      </Container>
+
+      <Container>
+        <p className="display-5">Where are your merger's</p>
+        <MapContainer
+          style={{
+            height: "70vh",
+            marginTop: "3vh",
+            borderRadius: "10px",
+          }}
+          center={[-15.363402, 28.319056]} //lat, lng
+          zoom={12}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {points &&
+            points.map((post, index) => {
+              if (post.latitude !== undefined && post.longitude !== undefined) {
+                let { latitude, longitude } = post;
+
+                // Check if the marker position is already occupied
+                while (isMarkerOccupied(latitude, longitude)) {
+                  latitude += generateOffset();
+                  longitude += generateOffset();
+                }
+
+                const key = generateMarkerKey(latitude, longitude);
+                markerPositions[key] = true;
+
+                return (
+                  <Marker
+                    key={index}
+                    position={[latitude, longitude]}
+                  >
+                    <Popup>
+                      <h4 className="text-center">{post.text}</h4>
+                      <img
+                        src={post.imageUrl}
+                        alt="merge voices"
+                        style={{ width: "20vh", padding: "0" }}
+                      />
+                    </Popup>
+                  </Marker>
+                );
+              }
+              return null;
+            })}
+        </MapContainer>
+      </Container>
+    </Container>
+  );
+}
+
+export default Explore;
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// User Interface
+import Feed from "./components/feed";
+import Profile from "./components/profile";
+import Booking from "./components/booking";
+// import Explore from "./components/map";
+
+function Router(props) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/booking" element={<Booking />} />
+        {/* <Route path="/help" element={<Help />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
